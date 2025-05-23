bf9f736a95fc32ac5b1e251fface28a749563e0a
tri-corv <trinidadcrvln@gmail.com>
2025-05-23 13:08:14 -0300

    Pantallas Administrador
    Creación de pantallas del lado administrador:
    -Panel principal
    -Editar productos
    -Perfil

---
+++ Editar productos/editar_productos.js
@@ -0,0 +1,11 @@
+//Funciones del menu
+document.getElementById('link-dashboard').onclick = () => window.location.href = '../Panel%20administrador/panel.html';
+document.getElementById('link-productos').onclick = () => window.location.href = '../Editar%20productos/editar_productos.html';
+document.getElementById('link-experiencias').onclick = () => window.location.href = '';
+document.getElementById('link-reservas').onclick = () => window.location.href = '';
+document.getElementById('link-reportes').onclick = () => window.location.href = '';
+document.getElementById('link-configuracion').onclick = () => window.location.href = '';
+document.getElementById('link-salir').onclick = () => window.location.href = '../Home/home.html';
+
+// Ícono USUARIO
+document.getElementById('icon-usuario').onclick = () => window.location.href = '../Perfil%20Administrador/perfilAdmin.html';
