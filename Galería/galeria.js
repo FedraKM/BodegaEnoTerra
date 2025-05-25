eb8ba9e84fb77ebd04d1f0029fba9de522863679
tri-corv <trinidadcrvln@gmail.com>
2025-05-25 12:14:59 -0300

    Nueva pantalla Galería
    Galería de fotos

---
+++ Galería/galeria.js
@@ -0,0 +1,10 @@
+// Funciones del menu
+document.getElementById('link-home').onclick = () => window.location.href = '../Home/home.html';
+document.getElementById('link-vinos').onclick = () => window.location.href = '../Vinos/vinos.html';
+document.getElementById('link-enoturismo').onclick = () => window.location.href = '../Enoturismo/enoturismo.html';
+document.getElementById('link-gastronomia').onclick = () => window.location.href = '../Restaurante/restaurante.html';
+document.getElementById('link-ubicacion').onclick = () => window.location.href = '../ubicacion/ubicacion.html';
+document.getElementById('link-contacto').onclick = () => window.location.href = '../Contacto/contacto.html';
+
+// Ícono USUARIO
+document.getElementById('icon-usuario').onclick = () => window.location.href = '../InicioSesion/iniciosesion.html';
